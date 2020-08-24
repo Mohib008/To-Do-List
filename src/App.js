6e735d99255baf9fdcc1500ca47bb9b104392fb7
Mohib Arsala <mohib.0008@gmail.com>
2020-08-24 11:12:04 -0400

    tachyons install

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+class App extends React.Component {
+  render() {
+    return(
+      <div className="App">
+      <header className="">
+        <form className="measure center">
+          <input className="pa2 input-rest ba bg-transparent hover-bg-black hover-white w-100" type="text" placeholder="Enter Text" />
+          <button className="f4 fw6 ph0 mh0" type="submit">add</button>
+        </form>
       </header>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
+
 export default App;
+

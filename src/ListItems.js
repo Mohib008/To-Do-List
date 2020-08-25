fe50205520c5df062197cbddd1460619e2cf1f62
Mohibullah Arsala <mohib-arsala@Mohibullahs-MacBook-Pro.local>
2020-08-25 00:28:06 -0400

    render error solved

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,19 +1,36 @@
 import React from "react";
 import "./listItems.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import FlipMove from "react-flip-move";
 
 function ListItems(props) {
         const items = props.items;
         const listItems = items && items.map(item => {
-            return <div className="list" key="item.key">
-                <p>{item.text}</p>
+            return <div className="list" key={item.key}>
+                <p>
+                <input type="text" 
+                id={item.text}
+                value = {item.text}
+                onChange ={
+                    (e) => {
+                        props.setUpdate(e.target.value, item.key)
+                    }
+                }
+                />
                 <span>
-                    <FontAwesomeIcon className="faicons" icon="trash" />
+                    <FontAwesomeIcon className="faicons" 
+                    icon="trash"
+                    onClick={ () => props.deleteItem(item.key)
+                    }/>
                 </span>
+                </p>
             </div>
         })
         return(
+            <FlipMove duration={500} easing="ease-in-out">
             <div>{listItems}</div>
+            </FlipMove>
+
     )
 }
 
